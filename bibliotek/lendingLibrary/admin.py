a80de4346ece1312cfc20259730aea947fd60f65
larry <lmoiola@gmail.com>
2019-08-06 22:25:37 -0700

    updating README.md to match models.py changes

--- bibliotek/lendingLibrary/admin.py
+++ bibliotek/lendingLibrary/admin.py
@@ -5,9 +5,11 @@ from .models import UserItem
 from .models import UserItemCheckout
 from .models import CheckoutStatus
 from .models import UserItemCategory
+from .models import UserItemCondition
 
 admin.site.register(User)
 admin.site.register(UserItem)
 admin.site.register(UserItemCheckout)
 admin.site.register(CheckoutStatus)
 admin.site.register(UserItemCategory)
+admin.site.register(UserItemCondition)
--- bibliotek/lendingLibrary/models.py
+++ bibliotek/lendingLibrary/models.py
@@ -20,14 +20,21 @@ class UserItemCategory(models.Model):
     def __str__(self):
         return self.name
 
+class UserItemCondition(models.Model):
+    name            = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class UserItem(models.Model):
     name            = models.CharField(max_length=200)
     description     = models.TextField(default="add some detail")
+    image_url       = models.CharField(default="https://lar-mo.com/images/lazy_placeholder.gif", max_length=200)
     type            = models.ForeignKey(UserItemCategory, on_delete=models.CASCADE)
-    availability    = models.BooleanField()
-    condition       = models.CharField(max_length=200)
+    condition       = models.ForeignKey(UserItemCondition, on_delete=models.CASCADE)
     replacement_cost = models.DecimalField(max_digits=6, decimal_places=2)
     owner           = models.ForeignKey(User, on_delete=models.CASCADE)
+    checked_out     = models.BooleanField()
     hidden          = models.BooleanField()
 
     def __str__(self):
