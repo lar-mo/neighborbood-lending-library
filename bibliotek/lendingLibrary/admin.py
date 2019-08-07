afb60fb193c77514c42780b5e5fb83715ad5a700
larry <lmoiola@gmail.com>
2019-08-07 11:11:30 -0700

    changes to models.py

--- bibliotek/lendingLibrary/admin.py
+++ bibliotek/lendingLibrary/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 
-from .models import User
 from .models import UserItem
 from .models import UserItemCheckout
-from .models import CheckoutStatus
+from .models import UserItemStatus
 from .models import UserItemCategory
 from .models import UserItemCondition
 
-admin.site.register(User)
 admin.site.register(UserItem)
 admin.site.register(UserItemCheckout)
-admin.site.register(CheckoutStatus)
+admin.site.register(UserItemStatus)
 admin.site.register(UserItemCategory)
 admin.site.register(UserItemCondition)
--- bibliotek/lendingLibrary/models.py
+++ bibliotek/lendingLibrary/models.py
@@ -1,14 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    name            = models.CharField(max_length=200)
-    email           = models.CharField(max_length=200)
-    phone_number    = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-class CheckoutStatus(models.Model):
+class UserItemStatus(models.Model):
     name            = models.CharField(max_length=200)
 
     def __str__(self):
@@ -30,23 +23,22 @@ class UserItem(models.Model):
     name            = models.CharField(max_length=200)
     description     = models.TextField(default="add some detail")
     image_url       = models.CharField(default="https://lar-mo.com/images/lazy_placeholder.gif", max_length=200)
-    type            = models.ForeignKey(UserItemCategory, on_delete=models.CASCADE)
-    condition       = models.ForeignKey(UserItemCondition, on_delete=models.CASCADE)
-    replacement_cost = models.DecimalField(max_digits=6, decimal_places=2)
-    owner           = models.ForeignKey(User, on_delete=models.CASCADE)
-    checked_out     = models.BooleanField()
-    hidden          = models.BooleanField()
+    type            = models.ForeignKey(UserItemCategory, on_delete=models.PROTECT)
+    condition       = models.ForeignKey(UserItemCondition, on_delete=models.PROTECT)
+    replacement_cost = models.FloatField()
+    owner           = models.ForeignKey(User, on_delete=models.PROTECT)
+    item_status     = models.ForeignKey(UserItemStatus, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
 
 class UserItemCheckout(models.Model):
-    user_item       = models.ForeignKey(UserItem, on_delete=models.CASCADE)
-    status          = models.ForeignKey(CheckoutStatus, on_delete=models.CASCADE)
+    user_item       = models.ForeignKey(UserItem, on_delete=models.PROTECT)
+    item_status     = models.ForeignKey(UserItemStatus, on_delete=models.PROTECT)
     request_date    = models.DateTimeField()
     checkout_date   = models.DateTimeField(default=None, blank=True, null=True)
     checkin_date    = models.DateTimeField(default=None, blank=True, null=True)
-    borrower        = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrower        = models.ForeignKey(User, on_delete=models.PROTECT)
     due_date        = models.DateTimeField(default=None, blank=True, null=True)
 
     def __str__(self):
