85ddecee1c4ce96e4cfdad1ec9d2a98a44b10d97
larry <lmoiola@gmail.com>
2019-11-23 18:12:48 -0800

    removing django_registration stuff

--- bibliotek/bibliotek/urls.py
+++ bibliotek/bibliotek/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     #remove lendingLibrary if hosted on it's own domain
     path('', include('lendingLibrary.urls')),
     path('admin/', admin.site.urls),
-    url(r'^accounts/', include('django_registration.backends.activation.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
